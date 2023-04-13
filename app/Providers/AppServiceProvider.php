9a5bd89c95955a40d44f14a7cc62c540ece7e6be
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-04-13 19:32:48 -0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Console\ClientCommand;
 use Laravel\Passport\Console\InstallCommand;
@@ -29,8 +30,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+
         Schema::defaultStringLength(191);
-         /*ADD THIS LINES*/
+        Paginator::useBootstrap();
+
+        setlocale(LC_TIME, 'pt-br');
+
+        Schema::defaultStringLength(191);
+
+        /*ADD THIS LINES*/
         $this->commands([
             InstallCommand::class,
             ClientCommand::class,
